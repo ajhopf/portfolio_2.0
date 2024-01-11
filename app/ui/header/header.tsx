90a02eba12abd9e61485e4ef03be60b906868bb7
André Hopf <andre.hopf@gmail.com>
2024-01-11 19:18:12 -0300

    Add logo, home image and header

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,33 +1,40 @@
 'use client';
 import Link from 'next/link';
-import styles from './header.module.css';
-import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import whiteLogo from './assets/white-logo.png';
+import greenLogo from './assets/green-logo.png';
+import Image from 'next/image';
 
 const links = [
-  { title: 'home', href: '/' },
-  { title: 'about', href: '/about' },
-  { title: 'projects', href: '/projects' }
+  { title: 'Home', href: '/' },
+  { title: 'About', href: '/about' },
+  { title: 'Projects', href: '/projects' }
 ];
 
 const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="flex justify-between items-center mx-2 h-[10vh] w-screen absolute">
-      <h1 className="text-green-400 text-2xl ml-10">André Hopf</h1>
+    <header
+      className={clsx(
+        'flex justify-between items-center px-10  h-[10vh] w-screen',
+        { 'absolute z-50': pathname === '/' }
+      )}
+    >
+      <Image className="h-[8vh] w-auto " src={greenLogo} alt="Hopf logo" />
       <nav>
         {links.map(link => {
           return (
             <Link
-              className={clsx('mx-2', {
-                'underline decoration-2 decoration-green-400':
+              className={clsx('mx-2 tracking-widest', {
+                'underline underline-offset-8 decoration-2 decoration-green-400':
                   pathname === link.href
               })}
               key={link.title}
               href={link.href}
             >
-              {link.title.toUpperCase()}
+              {link.title}
             </Link>
           );
         })}
--- app/ui/home/home.tsx
+++ app/ui/home/home.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image';
-import andre from './andre-hopf2.png';
+import andre from './andre-hopf.png';
 import styles from './home.module.css';
 
 const Home = () => {
   return (
     <div className="relative max-h-[90vh]">
       <Image
-        className={`${styles.image} max-h-[60vh] w-auto grayscale hover:grayscale-0 ease-in-out duration-700 ml-4`}
+        className={`${styles.image} h-[80vh] w-auto grayscale hover:grayscale-0 ease-in-out duration-700 ml-4 relative -top-4`}
         src={andre}
         alt="André Hopf"
       />
-      <div className="absolute bottom-1/4 right-0 ">
+      <div className="absolute bottom-1/4 right-0 w-1/3">
         <p className="pr-4 text-lg">
-          Hello, I&apos;m
+          Hey! I&apos;m
           <span className="text-green-400"> André Hopf</span>, Full-Stack
           Developer Based in Florianópolis
         </p>
